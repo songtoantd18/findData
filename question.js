2a0a469a125ce62aee8ca913a9e612dc8271a1ed
songtoantd18@gmail.com <songtoan>
2024-04-06 10:21:38 +0700

    lan2

---
+++ question.js
@@ -0,0 +1,207 @@
+listActress = [
+  {
+    name: "kitano mina",
+    id: "1",
+  },
+  {
+    name: "Hirose Hina",
+    id: "2",
+  },
+  {
+    name: "Satsuki Nao",
+    id: "3",
+  },
+  {
+    name: "Takashima Megumi",
+    id: "5",
+  },
+  {
+    name: "Sasaki Saki",
+    id: "7",
+  },
+  {
+    name: "Asano Kokoro",
+    id: "10",
+  },
+  {
+    name: "Satou Shio",
+    id: "11",
+  },
+  {
+    name: "Kasai Reona",
+    id: "12",
+  },
+  {
+    name: "Nanase Arisu",
+    id: "13",
+  },
+  {
+    name: "Kusunoki Erisa",
+    id: "14",
+  },
+  {
+    name: "Igarashi Natsu",
+    id: "16",
+  },
+  {
+    name: "Kasui Jun",
+    id: "17",
+  },
+  {
+    name: "Azusa Hikari",
+    id: "18",
+  },
+  {
+    name: "Asahi rio",
+    id: "19",
+  },
+  {
+    name: "Shiromine Miu",
+    id: "20",
+  },
+  {
+    name: "Hayama Sayuri",
+    id: "21",
+  },
+  {
+    name: "Kaede Karen",
+    id: "22",
+  },
+  {
+    name: "Kawakita Saika",
+    id: "23",
+  },
+  {
+    name: "Hirose Yuri",
+    id: "24",
+  },
+  {
+    name: "Nishino Emi",
+    id: "25",
+  },
+  {
+    name: "Kiyomi Reno",
+    id: "28",
+  },
+  {
+    name: "kuroshima-rei",
+    id: "29",
+  },
+  {
+    name: "Kaede Fuua",
+    id: "30",
+  },
+  {
+    name: "tsubasa Mai",
+    id: "31",
+  },
+  {
+    name: "Kamiki Rei",
+    id: "32",
+  },
+  {
+    name: "Kurumi Sakura",
+    id: "33",
+  },
+  {
+    name: "Itsukaichi Mei",
+    id: "35",
+  },
+  {
+    name: "Hayano Uta",
+    id: "36",
+  },
+  {
+    name: "Totsuka Ruu",
+    id: "37",
+  },
+  {
+    name: "Tsukino Kasumi",
+    id: "38",
+  },
+  {
+    name: "Kasumi Riko",
+    id: "39",
+  },
+  {
+    name: "Kasumi Haruka",
+    id: "40",
+  },
+  {
+    name: "Takara",
+    id: "41",
+  },
+  {
+    name: "Aoi Tsukasa",
+    id: "42",
+  },
+  {
+    name: "Nagarekawa Rio",
+    id: "43",
+  },
+  {
+    name: "Sannomiya Tsubaki",
+    id: "44",
+  },
+  {
+    name: "Ono Yuuko",
+    id: "45",
+  },
+  {
+    name: "Mino Suzume",
+    id: "46",
+  },
+  {
+    name: "Misaki Nanami",
+    id: "47",
+  },
+  {
+    name: "Suzumori Remu",
+    id: "48",
+  },
+  {
+    name: "ASAMI MIZUHATA",
+    id: "49",
+  },
+  {
+    name: "Itou Mayuki",
+    id: "50",
+  },
+  {
+    name: "Ashitaba Mitsuha",
+    id: "51",
+  },
+  {
+    name: "hinako-mori",
+    id: "52",
+  },
+  {
+    name: "sakino-mirai",
+    id: "53",
+  },
+  {
+    name: "Murakami Yuuka",
+    id: "54",
+  },
+  {
+    name: "Shiina Kokoha",
+    id: "55",
+  },
+  {
+    name: "Kurimiya Futaba",
+    id: "56",
+  },
+  {
+    name: "yuuhina-emiri",
+    id: "57",
+  },
+  {
+    name: "Murakami Yuuka",
+    id: "58",
+  },
+  {
+    name: "Yoshitaka Nene",
+    id: "60",
+  },
+];
+// api =https://62fbae6be4bcaf53518af2ed.mockapi.io/api/actress
